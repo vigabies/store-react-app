731890486679b131009f005a32f56057e52c61d1
Gabriela Silva <vigabiesdivision@gmail.com>
2023-08-14 10:45:29 -0300

    pages

---
+++ src/pages/carrinhos/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import topo from '../../../assets/topo.png';
+
+export default function Carrinho() {
+    return <Image source={topo}/>
+}
