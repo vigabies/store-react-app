9b372b525bf74d1a8732adbd0cd7945b6468b55a
Gabriela Silva <vigabiesdivision@gmail.com>
2023-08-16 15:27:31 -0300

    :bee:

--- src/pages/carrinhos/index.tsx
+++ src/pages/carrinhos/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Image, Dimensions, Text, View} from "react-native";
 
-import topo from '../../../assets/topo.png';
+import topo from "../../../assets/topo.png";
+
+//para saber o tamanho da tela e enquadrar a imagem de maneira correta
+const width = Dimensions.get("screen").width;
 
 export default function Carrinho() {
-    return <Image source={topo}/>
+  return (
+    <>
+      <Image source={topo} style={Styles.top} />
+      <Text style={Styles.title}>Detalhe do Carrinho</Text>
+    </>
+  );
 }
+
+const Styles = StyleSheet.create({
+  top: {
+    width: "100%",
+    height: (578 / 768) * width,
+  },
+
+  title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      lineHeight: 26,
+      width: "100%",
+      textAlign: "center",
+      color: "white", 
+      position: "absolute",
+      padding: "4%",
+  }
+});
