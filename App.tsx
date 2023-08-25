9730b8ad1c0abaa3cc5aa81a38707e71f2653d33
Gabriela Silva <vigabiesdivision@gmail.com>
2023-08-25 12:11:36 -0300

    things I missed

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
+import * as React from 'react';
 import { StyleSheet, Text, View, StatusBar, SafeAreaView } from 'react-native';
 import Carrinho from './src/pages/carrinhos';
 
-export default function App() {
-  return (
-    <SafeAreaView>
-      <StatusBar />
-      <Carrinho />
-      
-    </SafeAreaView>
-  );
-}
-
+import { useFonts,
+         Montserrat_400Regular,
+         Montserrat_700Bold,
+         Montserrat_400Regular_Italic
+} from "@expo-google-fonts/montserrat";
+  
+  export default function App() {
+    const [fontCarregada] = useFonts({
+      "MontserratRegular": Montserrat_400Regular,
+      "MontserratBold": Montserrat_700Bold,
+      "MontserratItalic": Montserrat_400Regular_Italic
+    })
+  
+    if (!fontCarregada) {
+      return null;
+    }
+  
+    return (
+      <SafeAreaView>
+        <StatusBar/>
+        <Carrinho/>
+      </SafeAreaView>
+    );
+  }
 
--- src/pages/carrinhos/index.tsx
+++ src/pages/carrinhos/index.tsx
@@ -1,23 +1,35 @@
-import React from "react";
-import { StyleSheet, Image, Dimensions, Text, View} from "react-native";
+import * as React from "react";
+import { StyleSheet, Image, Dimensions, Text, View } from "react-native";
 
 import topo from "../../../assets/topo.png";
+import logo from "../../../assets/logo.png";
 
 //para saber o tamanho da tela e enquadrar a imagem de maneira correta
 const width = Dimensions.get("screen").width;
 
 export default function Carrinho() {
   return (
-    <>
-      <Image source={topo} style={Styles.top} />
-      <Text style={Styles.title}>Detalhe do Carrinho</Text>
+    <View>
       <View>
-        <Text>Carrinho de compras</Text>
-        <Text>Fazenda Etec Cidade do Livro</Text>
-        <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-        <Text>R$ 40,00</Text>
+        <Image source={topo} style={Styles.top} />
+        <Text style={Styles.title}>Detalhe do Carrinho</Text>
       </View>
-    </>
+
+      <View style={Styles.carrinho}>
+        <Text style={Styles.titulocarrinho}>Carrinho de compras</Text>
+
+        <View style={Styles.fazenda}>
+          <Image style={Styles.imagemfazenda} source={logo} />
+          <Text style={Styles.nomefazenda}>Fazenda Etec Cidade do Livro</Text>
+        </View>
+
+        <Text style={Styles.descricao}>
+          Uma cesta com produtos selecionados cuidadosamente da fazenda direto
+          da cozinha
+        </Text>
+        <Text style={Styles.preco}>R$ 40,00</Text>
+      </View>
+    </View>
   );
 }
 
@@ -28,13 +40,56 @@ const Styles = StyleSheet.create({
   },
 
   title: {
-      fontSize: 24,
-      fontWeight: "bold",
-      lineHeight: 26,
-      width: "100%",
-      textAlign: "center",
-      color: "white", 
-      position: "absolute",
-      padding: "4%",
-  }
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 26,
+    width: "100%",
+    textAlign: "center",
+    color: "white",
+    position: "absolute",
+    padding: "4%",
+  },
+
+  titulocarrinho: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#464646",
+    fontFamily: "MontserratBold",
+  },
+
+  carrinho: {
+    paddingVertical: 8,
+    padidingHorizontal: 16,
+    marginLeft: 16,
+    marginRight: 16,
+  },
+
+  nomefazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontWeight: "bold",
+  },
+
+  imagemfazenda: {
+    width: 32,
+    height: 32,
+  },
+
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+
+  descricao: {
+    color: "#a3a3a3",
+    fontStyle: "italic",
+  },
+
+  preco: {
+    fontSize: 26,
+    lineHeight: 42,
+    color: "#2a9f85",
+    marginTop: 8,
+  },
 });
